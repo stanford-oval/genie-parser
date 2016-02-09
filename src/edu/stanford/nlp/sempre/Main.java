cb751ae507c8947ea16b9370366063b2083599f7
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-02-08 17:21:24 -0800

    Add StreamAPI porcelain layer
    To be used by ThingEngine to communicate with SEMPRE behind the
    scenes. A simple JSON based protocol to obtain the computed
    values of utterances.

--- src/edu/stanford/nlp/sempre/Main.java
+++ src/edu/stanford/nlp/sempre/Main.java
@@ -1,5 +1,6 @@
 package edu.stanford.nlp.sempre;
 
+import fig.basic.LogInfo;
 import fig.basic.Option;
 import fig.exec.Execution;
 
@@ -9,32 +10,43 @@ import fig.exec.Execution;
  * @author Percy Liang
  */
 public class Main implements Runnable {
-  @Option public boolean interactive = false;
-  @Option public boolean server = false;
+	@Option
+	public boolean interactive = false;
+	@Option
+	public boolean server = false;
+	@Option
+	public boolean streamapi = false;
 
-  public void run() {
-    Builder builder = new Builder();
-    builder.build();
+	public void run() {
+		Builder builder = new Builder();
+		builder.build();
 
-    Dataset dataset = new Dataset();
-    dataset.read();
+		Dataset dataset = new Dataset();
+		dataset.read();
 
-    Learner learner = new Learner(builder.parser, builder.params, dataset);
-    learner.learn();
+		Learner learner = new Learner(builder.parser, builder.params, dataset);
+		learner.learn();
 
-    if (server) {
-      Master master = new Master(builder);
-      Server server = new Server(master);
-      server.run();
-    }
+		if (server) {
+			Master master = new Master(builder);
+			Server server = new Server(master);
+			server.run();
+		}
 
-    if (interactive) {
-      Master master = new Master(builder);
-      master.runInteractivePrompt();
-    }
-  }
+		if (interactive) {
+			Master master = new Master(builder);
+			master.runInteractivePrompt();
+		}
 
-  public static void main(String[] args) {
-    Execution.run(args, "Main", new Main(), Master.getOptionsParser());
-  }
+		if (streamapi) {
+			Master master = new Master(builder);
+			StreamAPI stream = new StreamAPI(master, builder);
+			stream.run();
+		}
+	}
+
+	public static void main(String[] args) {
+		LogInfo.writeToStdout = false;
+		Execution.run(args, "Main", new Main(), Master.getOptionsParser());
+	}
 }
