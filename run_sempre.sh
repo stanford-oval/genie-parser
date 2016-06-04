4f331defc1bdd2dc805fe3a81fa4da53a3c0bda4
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-06-04 14:04:01 -0700

    Fix init file

--- run_sempre.sh
+++ run_sempre.sh
@@ -4,6 +4,7 @@
 # I have them because they help the parser ignore too many
 # derivation that use $StringValue (which is a catch all)
 exec java -ea -Dmodules=core,corenlp,overnight,thingtalk,api \
+              -Djava.library.path=jni \
               -cp 'libsempre/*:lib/*' \
               edu.stanford.nlp.sempre.api.APIServer \
               -LanguageAnalyzer corenlp.CoreNLPAnalyzer \
