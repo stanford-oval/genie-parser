1dd9261f0a658545efd52e203750a602fe99fa3f
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-07-24 14:08:36 +0200

    experiments...

--- run_cmd_sabrina_interactive.sh
+++ run_cmd_sabrina_interactive.sh
@@ -6,7 +6,7 @@
 # The overnight paper does not include "rule" features
 # I have them because they help the parser ignore too many
 # derivation that use $StringValue (which is a catch all)
-exec java -ea '-Dmodules=core,overnight,freebase,thingtalk' \
+exec java -Xmx12G -ea '-Dmodules=core,overnight,freebase,thingtalk' \
               '-cp' 'libsempre/*:lib/*' \
               'edu.stanford.nlp.sempre.Main' \
               '-LanguageAnalyzer' 'corenlp.CoreNLPAnalyzer' \
@@ -16,18 +16,17 @@ exec java -ea '-Dmodules=core,overnight,freebase,thingtalk' \
               '-Builder.dataset' 'thingtalk.ThingpediaDataset' \
               '-Builder.valueEvaluator' 'thingtalk.JsonValueEvaluator' \
               '-Grammar.inPaths' 'sabrina/sabrina.grammar' \
-              '-FeatureExtractor.featureDomains' 'denotation' 'rule' \
               '-FeatureExtractor.featureComputers' 'overnight.OvernightFeatureComputer' \
               '-OvernightFeatureComputer.featureDomains' \
-              'match' 'ppdb' 'skip-bigram' 'root' 'alignment' 'lexical' \
+              'match' 'ppdb' 'skip-bigram' 'skip-ppdb' 'root' 'alignment' 'lexical' \
               'root_lexical' \
               '-FloatingParser.maxDepth' '12' \
               '-Parser.beamSize' '20' \
               '-Learner.maxTrainIters' '1' \
               '-SimpleLexicon.inPaths' 'sabrina/sabrina.lexicon' \
-              '-wordAlignmentPath' 'thingtalk/thingtalk.word_alignments.berkeley' \
-              '-phraseAlignmentPath' 'thingtalk/thingtalk.phrase_alignments' \
-              '-PPDBModel.ppdbModelPath' 'thingtalk/thingtalk-ppdb.txt' \
+              '-wordAlignmentPath' 'sabrina/sabrina.word_alignments.berkeley' \
+              '-phraseAlignmentPath' 'sabrina/sabrina.phrase_alignments' \
+              '-PPDBModel.ppdbModelPath' 'sabrina/sabrina-ppdb.txt' \
               '-ThingpediaDatabase.dbUrl' 'jdbc:mysql://thingengine.crqccvnuyu19.us-west-2.rds.amazonaws.com/thingengine' \
               '-ThingpediaDatabase.dbUser' 'sempre' \
               '-Main.interactive' 'true' \
