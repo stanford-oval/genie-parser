fa15bd6d0f7b9a3fde73639542d387e668979081
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-07-28 17:47:53 -0700

    Update corenlp to the latest version
    And add optional downloads for models for the other languages

--- run_sempre.sh
+++ run_sempre.sh
@@ -3,7 +3,7 @@
 # The overnight paper does not include "rule" features
 # I have them because they help the parser ignore too many
 # derivation that use $StringValue (which is a catch all)
-exec java -ea '-Dmodules=core,overnight,freebase,thingtalk' \
+exec java -ea '-Dmodules=core,corenlp,overnight,freebase,thingtalk' \
               '-cp' 'libsempre/*:lib/*' \
               'edu.stanford.nlp.sempre.APIServer' \
               '-LanguageAnalyzer' 'corenlp.CoreNLPAnalyzer' \
