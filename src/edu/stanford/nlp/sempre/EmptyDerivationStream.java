b68d7d6b3a7d604ae05f7ab0859135599f35c700
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-07-30 10:35:37 -0700

    Fix EmptyDerivationStream
    Botched merge from the location work. It's a miracle it was
    working.

--- src/edu/stanford/nlp/sempre/EmptyDerivationStream.java
+++ src/edu/stanford/nlp/sempre/EmptyDerivationStream.java
@@ -1,7 +1,5 @@
 package edu.stanford.nlp.sempre;
 
-import java.io.IOException;
-
 public final class EmptyDerivationStream implements DerivationStream {
 	@Override
 	public boolean hasNext() {
@@ -11,10 +9,6 @@ public final class EmptyDerivationStream implements DerivationStream {
 	@Override
 	public Derivation next() {
 		return null;
-	}
-
-	@Override
-	public void close() throws IOException {
 	}
 
 	@Override
