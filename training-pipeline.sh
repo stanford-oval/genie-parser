1683e81d78452883c22183e21ffc952bc7b6c43c
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-08-10 12:05:13 -0700

    Extract berkeley aligners portion of training pipeline into a new script
    It's helpful to run the training pipeline in a less automated fashion,
    because different portions of it need to run on different machines.

--- training-pipeline.sh
+++ training-pipeline.sh
@@ -12,23 +12,12 @@ set -x
 
 LANGUAGE_TAG=${LANGUAGE_TAG:-en}
 THINGENGINE=${THINGENGINE:-`pwd`/../thingengine-platform-cloud}
-BERKELEY_ALIGNER=${BERKELEY_ALIGNER:-`pwd`/../berkeleyaligner}
 
 # extract the canonicals from the db
 node ${THINGENGINE}/scripts/reconstruct_canonicals.js ./sabrina/sabrina.canonicals.tsv
 
-# run the berkeley aligner
-rm -fr ./berkeleyaligner.tmp
-mkdir -p ./berkeleyaligner.tmp/sabrina/test
-mkdir -p ./berkeleyaligner.tmp/sabrina/train
-cut -f1 ./sabrina/sabrina.canonicals.tsv > ./berkeleyaligner.tmp/sabrina/train/train.f
-cut -f2 ./sabrina/sabrina.canonicals.tsv > ./berkeleyaligner.tmp/sabrina/train/train.e
-( cd ./berkeleyaligner.tmp ; java -ea -jar $BERKELEY_ALIGNER/berkeleyaligner.jar ++../sabrina/sabrina.berkeleyaligner.conf )
-paste ./berkeleyaligner.tmp/output/training.f ./berkeleyaligner.tmp/output/training.e ./berkeleyaligner.tmp/output/training.align > ./sabrina/sabrina.word_alignments.berkeley.source
-
-# convert the berkeley aligner format to something sempre likes
-java -cp 'libsempre/*:lib/*' -Dmodules=core,corenlp,overnight,thingtalk edu.stanford.nlp.sempre.overnight.Aligner ./sabrina/sabrina.word_alignments.berkeley.source ./sabrina/sabrina.word_alignments.berkeley berkeley 2
-rm -fr ./berkeleyaligner.tmp
+# run the berkeley aligner with default parameters
+./run-berkeley-aligner.sh
 
 # here would optionally clean up the ppdb, but we don't yet
 
