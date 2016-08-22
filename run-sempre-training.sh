76e750b3560232e8a620e1a080355b9ce73aa717
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-08-22 14:14:11 -0700

    run-sempre-training: respect workdir/sempredir
    So that multiple sempre training processes can run in parallel
    in different working directories.

--- run-sempre-training.sh
+++ run-sempre-training.sh
@@ -4,13 +4,16 @@ set -e
 set -x
 
 LANGUAGE_TAG=${LANGUAGE_TAG:-en}
+WORKDIR=`pwd`
+SEMPREDIR=`dirname $0`
 
-rm -fr ./sempre.tmp
-java -Xmx12G -ea -Dmodules=core,corenlp,overnight,thingtalk \
+rm -fr ${WORKDIR}/sempre.tmp
+cp ${SEMPREDIR}/module-classes.txt .
+java -Xmx32G -ea -Dmodules=core,corenlp,overnight,thingtalk \
               -Djava.library.path=jni \
-              -cp 'libsempre/*:lib/*' \
+              -cp "${SEMPREDIR}/libsempre/*:${SEMPREDIR}/lib/*" \
               edu.stanford.nlp.sempre.Main \
-              -execDir ./sempre.tmp \
+              -execDir ${WORKDIR}/sempre.tmp \
               -LanguageAnalyzer corenlp.CoreNLPAnalyzer \
               -CoreNLPAnalyzer.entityRecognizers corenlp.PhoneNumberEntityRecognizer corenlp.EmailEntityRecognizer \
               -CoreNLPAnalyzer.yearsAsNumbers -CoreNLPAnalyzer.splitHyphens false \
@@ -20,7 +23,7 @@ java -Xmx12G -ea -Dmodules=core,corenlp,overnight,thingtalk \
               -Builder.valueEvaluator thingtalk.JsonValueEvaluator \
               -JavaExecutor.unpackValues false \
               -Builder.dataset thingtalk.ThingpediaDataset \
-              -Grammar.inPaths sabrina/sabrina.${LANGUAGE_TAG}.grammar \
+              -Grammar.inPaths ${SEMPREDIR}/sabrina/sabrina.${LANGUAGE_TAG}.grammar \
               -Grammar.tags floatingargs floatingnames floatingstrings \
               -FeatureExtractor.featureDomains rule \
               -FeatureExtractor.featureComputers overnight.OvernightFeatureComputer thingtalk.ThingTalkFeatureComputer \
@@ -34,19 +37,18 @@ java -Xmx12G -ea -Dmodules=core,corenlp,overnight,thingtalk \
               -Learner.maxTrainIters 3 \
               -Learner.reduceParserScoreNoise \
               -Parser.derivationScoreNoise 1 \
-              -wordAlignmentPath sabrina/sabrina.word_alignments.berkeley \
-              -phraseAlignmentPath sabrina/sabrina.phrase_alignments \
-              -PPDBModel.ppdbModelPath sabrina/sabrina-ppdb.txt \
+              -wordAlignmentPath ${WORKDIR}/sabrina/sabrina.word_alignments.berkeley.${LANGUAGE_TAG} \
+              -phraseAlignmentPath ${SEMPREDIR}/sabrina/sabrina.phrase_alignments \
+              -PPDBModel.ppdbModelPath ${SEMPREDIR}/sabrina/sabrina-ppdb.txt \
               -PPDBModel.ppdb false \
               -ThingpediaDatabase.dbUrl jdbc:mysql://thingengine.crqccvnuyu19.us-west-2.rds.amazonaws.com/thingengine \
               -ThingpediaDatabase.dbUser sempre \
-              -ThingpediaDataset.onlineLearnFile sabrina/sabrina.${LANGUAGE_TAG}.online_learn \
+              -ThingpediaDataset.onlineLearnFile ${WORKDIR}/sabrina/sabrina.${LANGUAGE_TAG}.online_learn \
               -ThingpediaDataset.languageTag ${LANGUAGE_TAG} \
               -BeamParser.executeAllDerivations true \
               -FloatingParser.executeAllDerivations true \
               "$@"
 
 # move the generated file where APIServer will know to look for
-cp sempre.tmp/params.3 ./sabrina/sabrina.${LANGUAGE_TAG}.params
-
-rm -fr ./sempre.tmp
+cp ${WORKDIR}/sempre.tmp/params.3 ${WORKDIR}/sabrina/sabrina.${LANGUAGE_TAG}.params
+rm -fr ${WORKDIR}/sempre.tmp
