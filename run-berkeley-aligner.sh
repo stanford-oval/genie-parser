b52e683a2404fb3cecad5f6377948c85944546b5
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-09-09 11:09:55 -0700

    Update run-berkeley-aligner script
    With the one actually used in production

--- run-berkeley-aligner.sh
+++ run-berkeley-aligner.sh
@@ -1,18 +1,42 @@
 #!/bin/sh
 
-LANGUAGE_TAG=${LANGUAGE_TAG:-en}
-BERKELEY_ALIGNER=${BERKELEY_ALIGNER:-`pwd`/../berkeleyaligner}
-CANONICALS=${1:-./sabrina/sabrina.canonicals.tsv}
+set -e
+set -x
 
-# run the berkeley aligner
+CANONICALS=${1:-./sabrina/sabrina.canonicals.tsv}
+SEMPREDIR=${2:-`pwd`}
+
+# prepare the berkeley aligner input
 rm -fr ./berkeleyaligner.tmp
 mkdir -p ./berkeleyaligner.tmp/sabrina/test
 mkdir -p ./berkeleyaligner.tmp/sabrina/train
-cut -f1 $CANONICALS > ./berkeleyaligner.tmp/sabrina/train/train.f
-cut -f2 $CANONICALS > ./berkeleyaligner.tmp/sabrina/train/train.e
-( cd ./berkeleyaligner.tmp ; java -ea -jar $BERKELEY_ALIGNER/berkeleyaligner.jar ++../sabrina/sabrina.berkeleyaligner.conf )
-paste ./berkeleyaligner.tmp/output/training.f ./berkeleyaligner.tmp/output/training.e ./berkeleyaligner.tmp/output/training.align > ./sabrina/sabrina.word_alignments.berkeley.source.${LANGUAGE_TAG}
+java -cp "${SEMPREDIR}/libsempre/*:${SEMPREDIR}/lib/*" \
+	-Dmodules=core,corenlp,overnight,thingtalk \
+	edu.stanford.nlp.sempre.overnight.Tokenizer \
+	${CANONICALS} \
+	./berkeleyaligner.tmp/sabrina/train/train.f \
+	./berkeleyaligner.tmp/sabrina/train/train.e \
+	${LANGUAGE_TAG}
+
+# run the berkeley aligner
+( cd ./berkeleyaligner.tmp ; java -ea -jar /opt/berkeleyaligner/berkeleyaligner.jar ++${SEMPREDIR}/sabrina/sabrina.berkeleyaligner.conf )
+paste ./berkeleyaligner.tmp/output/training.f \
+      ./berkeleyaligner.tmp/output/training.e \
+      ./berkeleyaligner.tmp/output/training.align > ./sabrina/sabrina.word_alignments.berkeley.source.${LANGUAGE_TAG}
 
 # convert the berkeley aligner format to something sempre likes
-java -cp 'libsempre/*:lib/*' -Dmodules=core,corenlp,overnight,thingtalk edu.stanford.nlp.sempre.overnight.Aligner ./sabrina/sabrina.word_alignments.berkeley.source.${LANGUAGE_TAG} ./sabrina/sabrina.word_alignments.berkeley.${LANGUAGE_TAG} berkeley 2
+java -cp "${SEMPREDIR}/libsempre/*:${SEMPREDIR}/lib/*" \
+	-Dmodules=core,corenlp,overnight,thingtalk \
+	edu.stanford.nlp.sempre.overnight.Aligner \
+	./sabrina/sabrina.word_alignments.berkeley.source.${LANGUAGE_TAG} \
+	./sabrina/sabrina.word_alignments.berkeley.${LANGUAGE_TAG} berkeley 2
 rm -fr ./berkeleyaligner.tmp
+
+# run the phrase aligner
+java -cp "${SEMPREDIR}/libsempre/*:${SEMPREDIR}/lib/*" \
+	-Dmodules=core,corenlp,overnight,thingtalk \
+	edu.stanford.nlp.sempre.overnight.PhraseAligner \
+	$CANONICALS \
+	./sabrina/sabrina.phrase_alignments.${LANGUAGE_TAG} \
+	./sabrina/sabrina.word_alignments.berkeley.${LANGUAGE_TAG} \
+	${LANGUAGE_TAG} 2
