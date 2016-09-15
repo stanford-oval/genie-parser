da8a829fd3f92aef506aa7e0eec8f67b6225d9e2
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-09-15 11:23:50 -0700

    Simplify the process of adding values to actions/queries/triggers
    Instead of doing it piecewise in three rules, have a semantic fn
    that just generates all possible (name, operator) pairs that
    bridge between an action and a value.
    
    This greatly reduces the depth of actions with arguments (which
    means we can cut on the floating parser depth), it reduces the
    derivations that are featurized redundantly, and it cleans up
    the code by removing ugly LambdaFormula manipulation.

--- src/edu/stanford/nlp/sempre/thingtalk/AddValueFn.java
+++ src/edu/stanford/nlp/sempre/thingtalk/AddValueFn.java
@@ -1,32 +1,127 @@
 package edu.stanford.nlp.sempre.thingtalk;
 
+import java.util.*;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
 import edu.stanford.nlp.sempre.*;
+import fig.basic.LispTree;
 
 public class AddValueFn extends SemanticFn {
+  private static List<String> OPERATORS = Lists.newArrayList("is", "contains", ">", "<", "has");
+  private static List<String> ACTION_OPERATORS = Collections.singletonList("is");
+
+  private boolean isAction;
+  private String withToken;
+  private List<String> opNames;
+
+  @Override
+  public void init(LispTree tree) {
+    super.init(tree);
+    isAction = tree.child(1).value.equals("action");
+    withToken = tree.child(2).value;
+
+    LispTree opList = tree.child(3);
+    opNames = new ArrayList<>();
+    for (LispTree op : opList.children) {
+      if (op.isLeaf())
+        opNames.add(op.value);
+      else
+        opNames.add(Joiner.on(' ').join(op.children));
+    }
+  }
+
   @Override
   public DerivationStream call(final Example ex, final Callable c) {
-    return new SingleDerivationStream() {
-      @Override
-      public Derivation createDerivation() {
-        Derivation left = c.child(0);
-        Derivation right = c.child(1);
+    return new AddValueStream(ex, c);
+  }
 
-        // FIXME check this is used correctly
+  private static boolean operatorOk(String paramType, String operator) {
+    switch (operator) {
+    case "is":
+      return !paramType.startsWith("Array(");
+    case "contains":
+      return paramType.equals("String");
+    case "has":
+      return paramType.startsWith("Array(");
+    case ">":
+    case "<":
+      return paramType.equals("Number") || paramType.startsWith("Measure(");
+    default:
+      return true;
+    }
+  }
 
-        LambdaFormula lf = (LambdaFormula) left.formula;
-        CallFormula cf = (CallFormula) lf.body;
-        ValueFormula<?> vf = (ValueFormula<?>) cf.args.get(1);
-        ParamNameValue param = (ParamNameValue) vf.value;
+  private class AddValueStream extends MultipleDerivationStream {
+    private final Example ex;
+    private final Callable callable;
+    private final ParametricValue invocation;
+    private final Iterator<String> argnameIter;
+    private String currentArgname;
+    private ListIterator<String> opIter;
 
-        String haveType = ThingTalk.typeFromValue(right.value);
-        if (!ArgFilterHelpers.typeOk(haveType, param.type, right.value) &&
-            !ArgFilterHelpers.typeOkArray(haveType, param.type, right.value))
-          return null;
+    public AddValueStream(Example ex, Callable callable) {
+      this.ex = ex;
+      this.callable = callable;
 
-        return new Derivation.Builder().withCallable(c).formula(Formulas.lambdaApply(lf, right.formula))
-            .type(SemType.anyType)
-            .createDerivation();
+      Derivation left = callable.child(0);
+      if (left.value == null || !(left.value instanceof ParametricValue))
+        throw new IllegalArgumentException("AddValueFn used incorrectly");
+
+      invocation = (ParametricValue) left.value;
+      argnameIter = invocation.name.argtypes.keySet().iterator();
+    }
+
+    @Override
+    public Derivation createDerivation() {
+      while (true) {
+        if (opIter == null) {
+          if (!argnameIter.hasNext())
+            return null;
+
+          currentArgname = argnameIter.next();
+          if (isAction && invocation.hasParamName(currentArgname))
+            continue;
+
+          List<String> opList = isAction ? ACTION_OPERATORS : OPERATORS;
+          opIter = opList.listIterator();
+        }
+        if (!opIter.hasNext()) {
+          opIter = null;
+          continue;
+        }
+
+        ParamNameValue param = new ParamNameValue(currentArgname, invocation.name.getArgType(currentArgname));
+
+        int opIndex = opIter.nextIndex();
+        String operator = opIter.next();
+        if (!operatorOk(param.type, operator))
+          continue;
+
+        Derivation left = callable.child(0);
+        Derivation right = callable.child(1);
+        Value toAdd = right.value;
+        String haveType = ThingTalk.typeFromValue(toAdd);
+
+        if (!ArgFilterHelpers.typeOk(haveType, param.type, toAdd) &&
+            !ArgFilterHelpers.typeOkArray(haveType, param.type, toAdd))
+          continue;
+
+        ParamValue pv = new ParamValue(param, haveType, operator, toAdd);
+
+        ParametricValue newInvocation = invocation.clone();
+        newInvocation.add(pv);
+
+        String canonical = left.canonicalUtterance + " " + withToken + " arg " +
+            invocation.name.getArgCanonical(currentArgname) + " " + opNames.get(opIndex) + " "
+            + right.canonicalUtterance;
+
+        Derivation.Builder bld = new Derivation.Builder().withCallable(callable)
+            .formula(new ValueFormula<>(newInvocation)).type(SemType.entityType)
+            .canonicalUtterance(canonical);
+        return bld.createDerivation();
       }
-    };
+    }
   }
 }
