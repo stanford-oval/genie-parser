8da0ffceba06962836dee6ded95fdc89c5fc1f8a
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-10-27 21:19:06 -0700

    Fix PhraseAligner
    The phrase aligner should operate on the actual alignments
    computed by the berkeley aligner instead of trying to reproduce
    them by matching.

--- src/edu/stanford/nlp/sempre/overnight/PhraseAligner.java
+++ src/edu/stanford/nlp/sempre/overnight/PhraseAligner.java
@@ -3,7 +3,6 @@ package edu.stanford.nlp.sempre.overnight;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import com.google.common.base.Joiner;
@@ -15,48 +14,15 @@ import edu.stanford.nlp.sempre.corenlp.CoreNLPAnalyzer;
 import edu.stanford.nlp.stats.ClassicCounter;
 import edu.stanford.nlp.stats.Counter;
 import edu.stanford.nlp.stats.Counters;
-import fig.basic.BipartiteMatcher;
 import fig.basic.MapUtils;
 
 public class PhraseAligner {
 
   private final Map<String, Counter<String>> model = new HashMap<>();
-  private final Aligner berkeleyAligner;
   private final String languageTag;
 
-  public PhraseAligner(String berkeleyFile, String languageTag) {
+  public PhraseAligner(String languageTag) {
     this.languageTag = languageTag;
-    berkeleyAligner = Aligner.read(berkeleyFile, languageTag);
-  }
-
-  private double[][] computeLexicalAlignmentMatrix(List<String> utteranceTokens, List<String> originalTokens) {
-    double[][] res = new double[utteranceTokens.size() + originalTokens.size()][utteranceTokens.size()
-        + originalTokens.size()];
-    //init with -infnty and low score on the diagonal
-    for (int i = 0; i < res.length - 1; i++) {
-      for (int j = i; j < res.length; j++) {
-        if (i == j) {
-          res[i][j] = 0d;
-          res[j][i] = 0d;
-        } else {
-          res[i][j] = -1000d;
-          res[j][i] = -1000d;
-        }
-      }
-    }
-
-    for (int i = 0; i < utteranceTokens.size(); ++i) {
-      for (int j = 0; j < originalTokens.size(); ++j) {
-        String inputToken = utteranceTokens.get(i);
-        String derivToken = originalTokens.get(j);
-
-        double product = berkeleyAligner.getCondProb(inputToken, derivToken)
-            * berkeleyAligner.getCondProb(derivToken, inputToken);
-        res[i][utteranceTokens.size() + j] = product;
-        res[utteranceTokens.size() + j][i] = product;
-      }
-    }
-    return res;
   }
 
   // This function implements the phrase-align algorithm from
@@ -64,7 +30,7 @@ public class PhraseAligner {
   // Franz Josef Och, Hermann Ney, ACL 2004
   //
   // as cited by the Overnight paper
-  public void phraseAlign(String exampleFile, int threshold) {
+  public void phraseAlign(String exampleFile) {
     model.clear();
 
     CoreNLPAnalyzer.opts.annotators = Lists.newArrayList("ssplit");
@@ -82,25 +48,38 @@ public class PhraseAligner {
       LanguageInfo utteranceInfo = analyzer.analyze(utterance);
       LanguageInfo originalInfo = analyzer.analyze(original);
 
-      double[][] alignmentMatrix = computeLexicalAlignmentMatrix(utteranceInfo.tokens, originalInfo.tokens);
-      BipartiteMatcher bMatcher = new BipartiteMatcher();
-      int[] assignment = bMatcher.findMaxWeightAssignment(alignmentMatrix);
+      int[] utteranceAlignment = new int[utteranceInfo.numTokens()];
 
+      // we use -1 to mean "not aligned"
+      for (int i = 0; i < utteranceAlignment.length; i++)
+          utteranceAlignment[i] = -1;
+      int[] originalAlignment = new int[originalInfo.numTokens()];
+      for (int i = 0; i < originalAlignment.length; i++)
+          originalAlignment[i] = -1;
+      
+      String[] alignInfo = phrases[2].split(" ");
+      for (String alignToken : alignInfo) {
+        String[] tokens = alignToken.split("-");
+        int utteranceToken = Integer.valueOf(tokens[0]);
+        int originalToken = Integer.valueOf(tokens[1]);
+
+        utteranceAlignment[utteranceToken] = originalToken;
+        originalAlignment[originalToken] = utteranceToken;
+      }
+      
       for (int i1 = 0; i1 < utteranceInfo.numTokens(); i1++) {
         for (int i2 = i1+1; i2 <= utteranceInfo.numTokens(); i2++) {
           // span utterance tokens [i1, i2)
-          
+
           // TP = target-phrase
           // (terminology from the paper)
-          int[] TP = new int[i2-i1];
-          // we use -1 to mean "not aligned"
-          for (int k = 0; k < i2-i1; k++) {
-            TP[k] = assignment[i1+k] >= utteranceInfo.numTokens() ? assignment[i1+k] - utteranceInfo.numTokens() : -1;
-          }
-          
+          int[] TP = new int[i2 - i1];
+          for (int k = 0; k < i2 - i1; k++)
+            TP[k] = utteranceAlignment[i1 + k];
+
           int jmin = -1, jmax = -1;
           boolean anyAligned = false;
-          for (int k = 0; k < i2-i1; k++) {
+          for (int k = 0; k < i2 - i1; k++) {
             if (TP[k] >= 0) {
               anyAligned = true;
               jmin = TP[k];
@@ -127,10 +106,8 @@ public class PhraseAligner {
             continue;
 
           int[] SP = new int[jmax - jmin + 1];
-          for (int k = 0; k < jmax - jmin + 1; k++) {
-            SP[k] = assignment[utteranceInfo.numTokens() + jmin + k] != utteranceInfo.numTokens() + jmin + k
-                ? assignment[utteranceInfo.numTokens() + jmin + k] : -1;
-          }
+          for (int k = 0; k < jmax - jmin + 1; k++)
+            SP[k] = originalAlignment[jmin + k];
 
           boolean inSourceSpan = true;
           for (int k = 0; k < jmax - jmin + 1; k++) {
@@ -147,7 +124,6 @@ public class PhraseAligner {
           // ignore 1 to 1 mappings (handled by the berkeley aligner files instead)
           if (i2 - i1 == 1 && jmax - jmin == 0)
             continue;
-
           hit(Joiner.on(' ').join(utteranceInfo.tokens.subList(i1, i2)),
               Joiner.on(' ').join(originalInfo.tokens.subList(jmin, jmax + 1)));
         }
@@ -155,9 +131,6 @@ public class PhraseAligner {
       
       System.out.printf("processed %s to %s\n", utterance, original);
     }
-
-    // do not normalize
-    applyThreshold(threshold);
   }
 
   //count every co-occurrence
@@ -185,15 +158,16 @@ public class PhraseAligner {
     }
   }
 
-  //args[0] - example file with utterance and original
+  //args[0] - example file with utterance, original and alignment
   //args[1] - output file
-  //args[2] - berkeley align file
-  //args[3] - language tag
-  //args[4] - threshold
+  //args[2] - language tag
+  //args[3] - threshold
   public static void main(String[] args) {
-    PhraseAligner aligner = new PhraseAligner(args[2], args[3]);
-    int threshold = Integer.parseInt(args[4]);
-    aligner.phraseAlign(args[0], threshold);
+    PhraseAligner aligner = new PhraseAligner(args[2]);
+    int threshold = Integer.parseInt(args[3]);
+    aligner.phraseAlign(args[0]);
+    // do not normalize
+    aligner.applyThreshold(threshold);
     try {
       aligner.saveModel(args[1]);
     } catch (IOException e) {
