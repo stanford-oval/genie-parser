a045cad4d95166d97b9d15f980ae50c359463ea8
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-11-04 00:08:12 -0700

    Kill floatingstrings in the interactive shell

--- run_cmd_sabrina_interactive.sh
+++ run_cmd_sabrina_interactive.sh
@@ -17,7 +17,7 @@ exec java -Xmx12G -ea -Dmodules=core,corenlp,overnight,thingtalk \
               -JavaExecutor.unpackValues false \
               -Builder.dataset thingtalk.ThingpediaDataset \
               -Grammar.inPaths sabrina/sabrina.en.grammar \
-              -Grammar.tags floatingargs floatingnames floatingstrings \
+              -Grammar.tags floatingargs floatingnames \
               -FeatureExtractor.featureComputers overnight.OvernightFeatureComputer thingtalk.ThingTalkFeatureComputer \
               -OvernightFeatureComputer.featureDomains \
               match ppdb skip-bigram skip-ppdb root alignment lexical \
