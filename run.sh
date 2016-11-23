3c791a6dcb01a2a0b35e863eddc2ec0a331194a6
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-11-22 23:02:59 -0800

    Restructure starting scripts
    Move all common options to a common configuration file, so that
    we don't run the three modes with different options all the time.
    
    There is now a single run.sh that takes a mode as the first
    argument. Unfortunately, training is a bit special because it
    runs with a temporary workdir, so it still has a wrapper script
    that generates the configuration file.

---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -x
+
+usage() {
+	echo "run.sh: [interactive|training|server] [extra SEMPRE args...]"
+}
+
+MODE=$1
+shift
+
+EXTRA_ARGS="$@"
+
+JAVA=${JAVA:-java}
+BASE_ARGS="-ea $JAVA_ARGS -Dmodules=core,corenlp,overnight,thingtalk,api -cp libsempre/*:lib/*"
+
+case $MODE in
+	interactive)
+		TARGET=edu.stanford.nlp.sempre.Main
+		MODE_ARGS="++sabrina/sabrina.interactive.conf"
+		;;
+	training)
+		TARGET=edu.stanford.nlp.sempre.Main
+		MODE_ARGS="++sabrina/sabrina.training.conf"
+		;;
+	server)
+		TARGET=edu.stanford.nlp.sempre.api.APIServer
+		MODE_ARGS="++sabrina/sabrina.server.conf"
+		;;
+	interactive)
+		usage
+		exit 1
+		;;
+esac
+
+exec ${JAVA} ${BASE_ARGS} ${TARGET} ${MODE_ARGS} ${EXTRA_ARGS}
