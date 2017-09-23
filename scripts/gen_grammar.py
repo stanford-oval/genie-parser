4c6f562b5d6da0198e08fe3d95d52e06b15a5585
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-09-23 12:00:17 -0700

    Rewrite gen_grammar script to use the new Thingpedia snapshot API
    To make thingpedia download reproducible, which means that NN
    results don't depend on the mess that thingpedia contributors
    do on their own.

--- scripts/gen_grammar.py
+++ scripts/gen_grammar.py
@@ -15,37 +15,38 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import MySQLdb
-import MySQLdb.cursors
 import sys
 import json
 import os
+import urllib.request
+import ssl
 
 def main():
-    conn = MySQLdb.connect(user='sempre', passwd=sys.argv[1],
-                           db='thingengine',
-                           use_unicode=True,
-                           charset='utf8mb4',
-                           host='thingengine.crqccvnuyu19.us-west-2.rds.amazonaws.com',
-                           ssl=dict(ca=os.path.join(os.path.dirname(__file__), '../data/thingpedia-db-ca-bundle.pem')))
-    cursor = conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-    cursor.execute("select kind, name, channel_type, argnames, types from device_schema ds, device_schema_channels dsc where ds.id = dsc.schema_id and dsc.version = ds.developer_version and kind_type <> 'global'")
-    for row in cursor.fetchall():
-        print(row['channel_type'], 'tt:' + row['kind'] + '.' + row['name'], end=' ')
-        argnames = json.loads(row['argnames'])
-        argtypes = json.loads(row['types'])
-        for argname, argtype in zip(argnames, argtypes):
-            print(argname, argtype, end=' ')
-        print()
-        
-    cursor = conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-    cursor.execute("select kind from device_schema where kind_type <> 'global'")
-    for row in cursor.fetchall():
-        print('device', 'tt-device:' + row['kind'])
-        
-    cursor = conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-    cursor.execute("select id from entity_names where not is_well_known")
-    for row in cursor.fetchall():
-        print('entity', row['id'])
-        
+    snapshot = int(sys.argv[1])
+    thingpedia_url = os.getenv('THINGPEDIA_URL', 'https://thingpedia.stanford.edu/thingpedia')
+    ssl_context = ssl.create_default_context()
+
+    with urllib.request.urlopen(thingpedia_url + '/api/snapshot/' + str(snapshot), context=ssl_context) as res:
+        data = json.load(res)['data']
+        for device in data:
+            if device['kind_type'] == 'global':
+                continue
+            print('device', 'tt-device:' + device['kind'])
+            def do_type(dictionary, channel_type):
+                for name, channel in dictionary.items():
+                    print(channel_type, 'tt:' + device['kind'] + '.' + name, end=' ')
+                    for argname, argtype in zip(channel['args'], channel['types']):
+                        print(argname, argtype, end=' ')
+                    print()
+            do_type(device['triggers'], 'trigger')
+            do_type(device['queries'], 'query')
+            do_type(device['actions'], 'action')
+
+    with urllib.request.urlopen(thingpedia_url + '/api/entities?snapshot=' + str(snapshot), context=ssl_context) as res:
+        data = json.load(res)['data']
+        for entity in data:
+            if entity['is_well_known'] == 1:
+                continue
+            print('entity', entity['type'])
+
 main()
--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -17,7 +17,7 @@
 
 # Usage:
 #
-# ./scripts/prepare.sh ${WORKDIR} ${DATABASE_PW}
+# ./scripts/prepare.sh ${WORKDIR} ${SNAPSHOT}
 
 download_glove() {
 wget http://nlp.stanford.edu/data/glove.42B.300d.zip -O ${WORKDIR}/glove.42B.300d.zip
@@ -30,7 +30,7 @@ set -x
 set -e
 
 WORKDIR=${1:-.}
-DATABASE_PW=${DATABASE_PW:-$2}
+SNAPSHOT=${2:-"-1"}
 EMBED_SIZE=${3:-300}
 srcdir=`dirname $0`/..
 srcdir=`realpath ${srcdir}`
@@ -39,7 +39,7 @@ GLOVE=${GLOVE:-$WORKDIR/glove.txt}
 
 test -f ${GLOVE} || download_glove
 
-python3 ${srcdir}/scripts/gen_grammar.py "${DATABASE_PW}" > ${WORKDIR}/thingpedia.txt
+python3 ${srcdir}/scripts/gen_grammar.py "${SNAPSHOT}" > ${WORKDIR}/thingpedia.txt
 cat ${DATASET}/*.tsv | cut -f2 | tr " " "\n" | sort -u > ${WORKDIR}/input_words.txt
 python3 ${srcdir}/scripts/trim_embeddings.py ${WORKDIR}/input_words.txt ${EMBED_SIZE} < ${GLOVE} > ${WORKDIR}/embeddings-${EMBED_SIZE}.txt
 cp ${srcdir}/data/default.conf ${WORKDIR}/default.conf
