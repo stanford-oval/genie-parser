ba67a4c80af55084f0ea3eedeececeeab0e26750
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-05-23 15:09:36 -0700

    fix run interactive with beam decode

--- run_interactive.py
+++ run_interactive.py
@@ -120,16 +120,13 @@ def run():
                         prediction = dict()
                         for key in predictions:
                             prediction[key] = predictions[key][0,0]
-                            #raw_prediction[key] = raw_predictions.rnn_output[key][0]
                         primary_prediction = prediction[config.grammar.primary_output]
                         index, = np.where(primary_prediction == config.grammar.end)
                         if len(index):
                             for key in predictions:
                                 prediction[key] = prediction[key][:index[0]+1]
-                                #raw_prediction[key] = raw_prediction[key][:index[0]+1]
                             primary_prediction = primary_prediction[:index[0]+1]
                         for key in prediction:
-                            #print(key, '=', raw_prediction[key])
                             print(key, '=>', prediction[key])
                         config.grammar.print_prediction(sentence, prediction)
                         if len(predictions[config.grammar.primary_output][0]) == 1:
@@ -138,9 +135,12 @@ def run():
                             except (KeyError, TypeError, IndexError, ValueError):
                                 print('failed to predict')
                         else:
-                            for i, beam in enumerate(predictions[0]):
+                            for i, beam in enumerate(predictions[config.grammar.primary_output][0]):
+                                beam_prediction = dict()
+                                for key in predictions:
+                                    beam_prediction[key] = predictions[key][0,i]
                                 try:
-                                    print('beam', i, ' '.join(config.grammar.reconstruct_program(sentence, beam)))
+                                    print('beam', i, ' '.join(config.grammar.reconstruct_program(sentence, beam_prediction)))
                                 except (KeyError, TypeError, IndexError, ValueError):
                                     print('beam', i, 'failed to predict')
 
