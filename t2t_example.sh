9af028b177ade3072d710eb9731e5dab70ef93f8
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-05-28 13:25:58 -0700

    modify t2t example

--- t2t_example.sh
+++ t2t_example.sh
@@ -9,10 +9,10 @@ TRAIN_DIR=$HOME/t2t_train/$PROBLEM/$MODEL-$HPARAMS
 mkdir -p $DATA_DIR $TMP_DIR $TRAIN_DIR
 
 # Generate data
-t2t-datagen \
-  --data_dir=$DATA_DIR \
-  --tmp_dir=$TMP_DIR \
-  --problem=$PROBLEM
+#t2t-datagen \
+#  --data_dir=$DATA_DIR \
+#  --tmp_dir=$TMP_DIR \
+#  --problem=$PROBLEM
 
 # Train
 # *  If you run out of memory, add --hparams='batch_size=1024'.
