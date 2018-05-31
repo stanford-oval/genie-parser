4ee68054c4ec3139bf1fd23ebc2d33e0f1f11a84
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-05-30 20:02:56 -0700

    fixed some typos in formatting data

--- t2t_format_data.py
+++ t2t_format_data.py
@@ -10,42 +10,43 @@ args = parser.parse_args()
 
 WORKDIR = './../workdir'
 DATASET = './../dataset'
-DIR = os.path.join(DATASET, '/t2t_dir')
+DIR = os.path.join(DATASET, 't2t_dir')
 
 if not os.path.exists(DIR):
     os.makedirs(DIR)
 
-with open(os.path.join(DIR, '/t2t_train_x'), 'w') as f1:
-    with open(os.path.join(DIR, '/t2t_train_y'), 'w') as f2:
-        with open(os.path.join(DATASET, '/', args.train_tsv), 'r') as f:
+with open(os.path.join(DIR, 't2t_train_x'), 'w') as f1:
+    with open(os.path.join(DIR, 't2t_train_y'), 'w') as f2:
+        with open(os.path.join(DATASET, args.train_tsv), 'r') as f:
             for line in f.readlines():
                 parts = re.split(r'\t+', line)
                 f1.write(parts[1] + '\n')
                 f2.write(parts[2])
 
-with open(os.path.join(DIR, '/t2t_test_x'), 'w') as f1:
-    with open(os.path.join(DIR, '/t2t_test_y'), 'w') as f2:
-        with open(os.path.join(DATASET, '/', args.test_tsv), 'r') as f:
+with open(os.path.join(DIR, 't2t_test_x'), 'w') as f1:
+    with open(os.path.join(DIR, 't2t_test_y'), 'w') as f2:
+        with open(os.path.join(DATASET, args.test_tsv), 'r') as f:
             for line in f.readlines():
                 parts = re.split(r'\t+', line)
                 f1.write(parts[1] + '\n')
                 f2.write(parts[2])
 
-with open(DIR + '/t2t_dev_x', 'w') as f1:
-    with open(DIR + '/t2t_dev_y', 'w') as f2:
-        with open(DATASET + '/' + args.dev_tsv, 'r') as f:
+with open(os.path.join(DIR, 't2t_dev_x'), 'w') as f1:
+    with open(os.path.join(DIR, 't2t_dev_y'), 'w') as f2:
+        with open(os.path.join(DATASET, args.dev_tsv), 'r') as f:
             for line in f.readlines():
                 parts = re.split(r'\t+', line)
                 f1.write(parts[1] + '\n')
                 f2.write(parts[2])
 
-if not os.path.exists(os.path.join(DATASET, '/t2t_data')):
-    os.makedirs(os.path.join(DATASET, '/t2t_data'))
+if not os.path.exists(os.path.join(DATASET, 't2t_data')):
+    os.makedirs(os.path.join(DATASET, 't2t_data'))
 
-with open(os.path.join(DATASET, '/t2t_data/all_words.txt'), 'w') as f:
-    with open(os.path.join(WORKDIR, '/input_words.txt'), 'r') as read:
+with open(os.path.join(DATASET, 't2t_data/all_words.txt'), 'w') as f:
+    with open(os.path.join(WORKDIR, 'input_words.txt'), 'r') as read:
         lines = read.readlines()
         f.writelines(lines)
-    with open(os.path.join(WORKDIR, '/output_words.txt'), 'r') as write:
+    with open(os.path.join(WORKDIR, 'output_words.txt'), 'r') as write:
         lines = write.readlines()
         f.writelines(lines)
+    f.writelines(['UNK\n'])
