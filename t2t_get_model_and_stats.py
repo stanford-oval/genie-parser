03241d5ee3149a82f928c1d3c010103ff04cb259
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-06-03 14:05:22 -0700

    add print only option to get_model_and_stats

--- t2t_get_model_and_stats.py
+++ t2t_get_model_and_stats.py
@@ -8,6 +8,7 @@ LABELS = ['step', 'loss', 'accuracy', 'sequence accuracy', 'top 5 accuracy', 'ap
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--clean", action='store_true')
+parser.add_argument("--print-only", action='store_true')
 parser.add_argument("--rank-seq-acc", action='store_true')
 parser.add_argument("--model-dir", default='/home/gcampagn/workdir/t2t_train/parse_almond/transformer-transformer_base_single_gpu')
 parser.add_argument("--file", default='transformer_output.txt')
@@ -15,15 +16,16 @@ parser.add_argument("--outdir", default='/home/gcampagn/workdir/model_and_stats/
 parser.add_argument("--outfile", default='transformer_output.csv')
 args = parser.parse_args()
 
-if not os.path.exists(args.outdir):
+if not os.path.exists(args.outdir) and not args.print_only:
     os.makedirs(args.outdir)
 
 with open(args.file, 'r') as f:
     output = f.read()
-    if args.clean:
-        os.rename(args.file, os.path.join(args.outdir, args.file))
-    else:
-        copyfile(args.file, os.path.join(args.outdir, args.file))
+    if not args.print_only:
+        if args.clean:
+            os.rename(args.file, os.path.join(args.outdir, args.file))
+        else:
+            copyfile(args.file, os.path.join(args.outdir, args.file))
 
 pattern  = re.compile("INFO:tensorflow:Saving dict for global step.+?(?=INFO)", re.DOTALL)
 best_acc = 0
@@ -31,8 +33,9 @@ best_step = -1
 best_stats = None
 
 with open(os.path.join(args.outdir, args.outfile), 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(LABELS)
+    if not args.print_only:
+        writer = csv.writer(f)
+        writer.writerow(LABELS)
     print('\t'.join(LABELS))
     for match in re.findall(pattern, output):
         step = re.search("(?<=global_step = ).+?(?=,)", match, re.DOTALL).group(0)
@@ -56,11 +59,12 @@ with open(os.path.join(args.outdir, args.outfile), 'w') as f:
         row = [int(step), float(loss), float(acc), float(seq_acc), float(acc_t5), float(bleu), float(perp), float(rouge_f2), float(rouge_fL)]
         row = [str(num) for num in row]
         print('\t'.join(row).strip())
-        writer.writerow(row)
+        if not args.print_only:
+            writer.writerow(row)
 
 if best_step != -1:
     print('Best step was {} with accuracy {}'.format(best_step, best_acc))
-    if args.clean:
+    if args.clean and not args.print_only:
         model_pattern = 'model.ckpt-'
         pattern = model_pattern + best_step + '\.'
         DIR = args.model_dir
@@ -72,6 +76,6 @@ if best_step != -1:
             else:
                 os.rename(os.path.join(DIR, f), os.path.join(args.outdir, f))
 
-        print('In out directory ', args.outdir)
+        print('In out directory, ', args.outdir)
         for f in os.listdir(args.outdir):
             print(f)
