41b820de89f85f7fd4ab0d9446b2d080528809a7
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-09-20 21:29:20 -0700

    travis: add automatic deployment when pushing to production

---
+++ travis/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -x
+
+srcdir=`dirname $0`
+ssh -o IdentityFile=$srcdir/id_rsa.autodeploy autodeploy@parmesan.stanford.edu
+ssh -o IdentityFile=$srcdir/id_rsa.autodeploy autodeploy@almond-nl.stanford.edu
