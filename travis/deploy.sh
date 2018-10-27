b23d40fcfda1c3f562b42ac59a48209217ea1d62
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-10-26 23:51:36 -0700

    travis: deploy to the right servers

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -5,5 +5,5 @@ set -o pipefail
 set -x
 
 srcdir=`dirname $0`
-ssh -o IdentityFile=$srcdir/id_rsa.autodeploy autodeploy@parmesan.stanford.edu
+ssh -o IdentityFile=$srcdir/id_rsa.autodeploy autodeploy@almond-training.stanford.edu
 ssh -o IdentityFile=$srcdir/id_rsa.autodeploy autodeploy@almond-nl.stanford.edu
