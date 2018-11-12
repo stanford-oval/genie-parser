2c7378644584758cec1b0c506018e5b79d2cf207
mehrad <mehrad@stanford.edu>
2018-11-12 15:32:57 -0800

    re-fix curriculum weights

--- luinet/tasks/semantic_parsing.py
+++ luinet/tasks/semantic_parsing.py
@@ -239,8 +239,8 @@ class SemanticParsingProblem(text_problems.Text2TextProblem,
         _type = tf.reshape(example["type"], ())
         zeros = tf.zeros_like(_type, dtype=tf.float32)
         synth_weight = tf.where(tf.equal(_type, 0), 0.1 + 0.9 * schedule, zeros)
-        para_weight = tf.where(tf.equal(_type, 1), 0.6 * (1 - schedule), zeros)
-        aug_weight = tf.where(tf.equal(_type, 2), 0.3 * (1 - schedule), zeros)
+        para_weight = tf.where(tf.equal(_type, 1), 0.6 * (1 - schedule + 1e-8), zeros)
+        aug_weight = tf.where(tf.equal(_type, 2), 0.3 * (1 - schedule + 1e-8), zeros)
 
         output_example = {
             "inputs": example["inputs"],
